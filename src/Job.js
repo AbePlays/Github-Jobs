4c23e8dfac6e1792c0828438646b63ac22eb2ae3
Abe <abhi.rawat456@gmail.com>
2020-07-25 07:15:48 +0530

    Added company markdown

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Badge } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
 
 function Job({ job }) {
   return (
@@ -13,7 +14,23 @@ function Job({ job }) {
                 {job.company}
               </span>
             </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: "break-all" }}>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
           </div>
+          <img
+            className="d-sm-none d-md-block"
+            height="50"
+            src={job.company_logo}
+            alt={job.company}
+          />
         </div>
       </Card.Body>
     </Card>
