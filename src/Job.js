de2d95fb284bceb051b1099a2cd8e25b523fe12e
Abe <abhi.rawat456@gmail.com>
2020-07-25 07:07:51 +0530

    Imported React-Bootstrap

--- src/Job.js
+++ src/Job.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 function Job({ job }) {
-  return <div>{job.title}</div>;
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} -{" "}
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
+            </Card.Title>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Job;
