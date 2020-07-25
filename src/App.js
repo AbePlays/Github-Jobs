b263759da84730311e4cff42fa5a04c6fc94d0d4
Abe <abhi.rawat456@gmail.com>
2020-07-25 07:36:10 +0530

    Added Pagination

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
 import Job from "./Job";
+import JobPagination from "./JobsPagination";
 
 function App() {
   const [params, setParams] = useState({});
@@ -10,11 +11,14 @@ function App() {
 
   return (
     <Container className="my-4">
+      <h1 className="mb-4">Github Jobs</h1>
+      <JobPagination page={page} setPage={setPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try reloading</h1>}
       {jobs.map((job) => {
         return <Job key={job.id} job={job}></Job>;
       })}
+      <JobPagination page={page} setPage={setPage} />
     </Container>
   );
 }
